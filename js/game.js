09f5969e1b701d7dd49d58e6737d1ff3801e1180
h3R <hermann@plass.biz>
2015-04-22 04:09:04 +0200

    Quite amount of things

---
+++ js/game.js
@@ -0,0 +1,9 @@
+/**
+ * Created by bleizzero on 22/04/2015.
+ */
+function game(){
+	console.log('Switched to \'game\' view.');
+	$('#index-content').load('views/game-layout.html');
+
+	var player_me = new Player();
+}
